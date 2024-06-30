d13360cc0beeb2f7b6bb112c9e9985f7936fc81c
Konsl <82901383+Konsl@users.noreply.github.com>
2024-06-30 19:22:57 +0200

    improve cover image selection

--- src/model.rs
+++ src/model.rs
@@ -1,15 +1,15 @@
-use id3::Content;
+use id3::frame::Picture;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct SongInfo  {
     pub title: String,
     pub artist: String,
     pub dance: String,
-    pub album_art: Option<Content>,
+    pub album_art: Option<Picture>,
 }
 
 impl SongInfo{
-    pub fn new(title: String, artist: String, dance: String, album_art: Option<Content>) -> Self {
+    pub fn new(title: String, artist: String, dance: String, album_art: Option<Picture>) -> Self {
         Self {
             title,
             artist,
