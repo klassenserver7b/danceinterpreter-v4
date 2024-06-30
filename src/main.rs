b5955514a6e0bac75fb6359f012ade65aa74a0fb
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-06-30 14:45:47 +0200

    moved ui code to seperate file

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,8 @@
 //pub mod dataloading;
 
+slint::include_modules!();
 fn main() {
     println!("Hello, world!");
 
-    Test::new().unwrap().run().unwrap();
-}
-
-slint::slint! {
-    import { TabWidget } from "std-widgets.slint";
-
-    export component Recipe inherits Window {
-        width: 200px;
-        height: 200px;
-        TabWidget {
-            Tab {
-                title: "First";
-                Rectangle { 
-                    background: orange;
-                    width: 50px;
-                }
-            }
-            Tab {
-                title: "Second";
-                Rectangle { background: pink; }
-            }
-        }
-    }
-
-    export component Test inherits Window {
-        Rectangle {
-            height: 200px;
-            theTabWidget := TabWidget {
-                width: parent.width;
-                Tab {
-                    title: "First";
-                    Rectangle { 
-                        background: orange;
-                    }
-                }
-                Tab {
-                    title: "Second";
-                    Rectangle { background: pink; }
-                }
-            }
-        }
-    }
+    MainWindow::new().unwrap().run().unwrap();
 }
