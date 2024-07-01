869488360629cac659ec9b4bd44a9d3ce2a317b5
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-07-01 23:14:24 +0200

    moved methods to super

--- src/dataloading/dataprovider/mod.rs
+++ src/dataloading/dataprovider/mod.rs
@@ -18,6 +18,21 @@ impl<'a, Item> VectorForwardBackward<'a, Item> {
             index: 0
         }
     }
+
+    #[allow(dead_code)]
+    pub fn current(&self) -> Option<&'a Item>{
+        self.vec.get(self.index)
+    }
+
+    #[allow(dead_code)]
+    pub fn at_index(&mut self, index: usize) -> Option<&'a Item> {
+        let current = self.vec.get(index);
+
+        if current.is_some() {
+            self.index = index;
+        }
+        current
+    }
 }
 
 impl<'a, Item> Iterator for VectorForwardBackward<'a, Item> {
--- src/dataloading/dataprovider/playlist_dataprovider.rs
+++ src/dataloading/dataprovider/playlist_dataprovider.rs
@@ -3,20 +3,3 @@ use crate::dataloading::dataprovider::VectorForwardBackward;
 
 
 pub type PlaylistDataProvider<'a> = VectorForwardBackward<'a, SongInfo>;
-
-impl<'a> PlaylistDataProvider<'a> {
-    #[allow(dead_code)]
-    pub fn current(&self) -> Option<&'a SongInfo>{
-        self.vec.get(self.index)
-    }
-
-    #[allow(dead_code)]
-    pub fn at_index(&mut self, index: usize) -> Option<&'a SongInfo> {
-        let current = self.vec.get(index);
-
-        if current.is_some() {
-            self.index = index;
-        }
-        current
-    }
-}
